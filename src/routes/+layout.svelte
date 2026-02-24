a2aeaa373e48640976048d4cc8bfcf5591e7cec2
Stefano Rapisarda <s.rapisarda86@gmail.com>
2026-02-24 09:37:05 +0100

    Umami set up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,11 @@
 
 <svelte:head>
 	<title>Stefano Rapisarda</title>
+	<script
+		defer
+		src="https://cloud.umami.is/script.js"
+		data-website-id="d1a35506-6105-48b7-ad59-4df5f850630e"
+	></script>
 </svelte:head>
 
 <Header />
